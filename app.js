37a041d34a641f98e49b3b48201c18951777a4a4
Isaac Giles <izzie717@gmail.com>
2021-08-11 11:10:58 -0400

    working with github api

--- app.js
+++ app.js
@@ -6,11 +6,26 @@ async function getRepos() {
     const result = await response.json();
 
     result.forEach(i=>{
-        const anchor = document.createElement("a");
-        anchor.href = "";
-        anchor.textContent = i.full_name;
-        projectListDiv.appendChild(anchor);
-        projectListDiv.appendChild(document.createElement("br"));
+
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("project_div");
+        const titleDiv = document.createElement("div");
+        titleDiv.classList.add("project_title_div");
+        const title = document.createElement("h1");
+        title.textContent = i.name;
+        titleDiv.appendChild(title);
+        projectDiv.appendChild(titleDiv);
+
+        const descriptionDiv = document.createElement("div");
+        titleDiv.classList.add("project_desc_div");
+        const description = document.createElement("p");
+        description.textContent = i.description;
+        titleDiv.appendChild(description);
+
+        projectListDiv.appendChild(projectDiv);
+
+        projectDiv.addEventListener("click", () => window.location=i.html_url);
+
     });
 
 
